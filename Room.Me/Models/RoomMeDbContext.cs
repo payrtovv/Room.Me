996c8f2def546346b50a05c879dcba2a25dba89d
Ayrton Villota <payrtovv@gmail.com>
2025-11-20 14:11:35 -0500

    Se agrego Model user y DbContext, se creo el controller para el access con la funcion para registrarse y se creo la migracion

---
+++ Room.Me/Models/RoomMeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Room.Me.Models
+{
+    public class RoomMeDbContext : DbContext
+    {
+        public RoomMeDbContext (DbContextOptions<RoomMeDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Room.Me/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+namespace Room.Me.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+
+    }
+}
