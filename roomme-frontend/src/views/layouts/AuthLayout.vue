1bb655ef7420447fbd87ba67734e0655c296524c
Mateo Torres <mateo.torres.duenas@udla.edu.ec>
2025-11-23 13:56:44 -0500

    Feature: Integracion del frontend

---
+++ roomme-frontend/src/views/layouts/AuthLayout.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container fluid class="pa-0 h-100 vh-100" >
+        <v-row no-gutters class="h-100">
+            <!--Image column-->
+            <v-col 
+                cols="12" 
+                md="7"
+                class="position-relative d-flex align-center justify-center"    
+            >
+                <v-img 
+                    src="@/assets/images/loginLayout.png"
+                    cover
+                    class="fill-height"
+                    style="position:absolute; top:0; left:0; width: 100%; height: 100%;"    
+                />
+                <h1 class="room-me-text">room.me</h1>
+                    
+            </v-col>
+            <v-col cols="12" md="5" class="h-100 d-flex align-center justify-center">
+            <v-sheet class="ma-8">
+                <RouterView />
+            </v-sheet>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<style scoped>
+
+.room-me-text{
+    font-size: 4rem;
+    font-weight: 700;
+    z-index: 1;
+    position: relative;
+}
+
+</style>
---
+++ roomme-frontend/src/views/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <v-app-bar
+        class="color-primary pa-4" flat
+    >
+
+        <v-spacer></v-spacer>
+        <v-btn
+            :ripple="false" 
+            class="text-none mr-2"
+            variant="flat"
+        >
+            Iniciar Sesión
+        </v-btn>
+        <v-btn
+            color="primary" 
+            :ripple="false" 
+            class="text-none"
+            variant="flat"
+        >
+            Registrarse
+        </v-btn>
+    </v-app-bar>
+</template>
