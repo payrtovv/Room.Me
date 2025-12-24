8baf15078795a90128b093272aa6cbcd629eb67d
Ayrton Villota <payrtovv@gmail.com>
2025-12-23 20:08:54 -0500

    Variables de entorno aniadidas

---
+++ Room.Me/Controllers/RoomsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Room.Me.Data;
+using Room.Me.Dtos;
+using Room.Me.Models;
+
+namespace Room.Me.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomsController : ControllerBase
+    {
+        private readonly RoomMeDbContext _Context;
+
+
+        public RoomsController(RoomMeDbContext context)
+        {
+            _Context = context;
+        }
+
+        [HttpPost("CreateRoom")]
+        public async Task<ActionResult> CreateRoom([FromBody] CreateRoomDto dto)
+        {
+            try
+            {
+
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var room = new Rooms
+                {
+                    Description = dto.Description,
+                    M2Space = dto.M2Space,
+                    Price = dto.Price,
+                    Direccion = dto.Direccion,
+                    City = dto.City,
+                    NearTransport = dto.NearTransport,
+                    NearCollege = dto.NearCollege,
+                    IncludesElectricity = dto.IncludesElectricity,
+                    IncludesWater = dto.IncludesWater,
+                    IncludesInternet = dto.IncludesInternet,
+                    IncludesGas = dto.IncludesGas,
+                    IncludesCleaning = dto.IncludesCleaning
+                };
+
+                _Context.Rooms.Add(room);
+                await _Context.SaveChangesAsync();
+
+
+                return Ok(new
+                {
+                    message = "Habitacion registrada exitosamente",
+                    room = new
+                    {
+                        room.Description,
+                        room.M2Space,
+                        room.Price,
+                        room.Direccion,
+                        room.City,
+                        room.NearTransport,
+                        room.NearCollege,
+                        room.IncludesElectricity,
+                        room.IncludesWater,
+                        room.IncludesInternet,
+                        room.IncludesGas,
+                        room.IncludesCleaning
+                    }
+                });
+
+            }
+            catch (Exception Ex)
+            {
+                return StatusCode(500, new
+                {
+                    message = "Ocurrió un error interno. Inténtalo más tarde.",
+                    error = Ex.Message
+                });
+            }
+        }
+    }
+}
+         
---
+++ Room.Me/Dtos/CreateRoomDto.cs
@@ -0,0 +1,31 @@
+﻿namespace Room.Me.Dtos
+{
+    public class CreateRoomDto
+    {
+
+        public String Description { get; set; }
+
+        //Tamanio en M2 dela habitacion
+        public float M2Space { get; set; }
+
+        public float Price { get; set; }
+
+        //Calles
+        public String Direccion { get; set; }
+
+        public String City { get; set; }
+
+        //Si esta cerca de transporte
+        public bool NearTransport { get; set; }
+
+        public bool NearCollege { get; set; }
+
+        //Servicios
+
+        public bool IncludesElectricity { get; set; }
+        public bool IncludesWater { get; set; }
+        public bool IncludesInternet { get; set; }
+        public bool IncludesGas { get; set; }
+        public bool IncludesCleaning { get; set; }
+    }
+}
