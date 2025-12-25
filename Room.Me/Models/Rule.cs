29633ca2212a46ae19188878a75b75aa207dc896
Ayrton Villota <payrtovv@gmail.com>
2025-12-25 17:32:42 -0500

    Funcion agregar Room, Agregar regla para la room y migracion

--- Room.Me/Models/Rule.cs
+++ Room.Me/Models/Rule.cs
@@ -6,7 +6,13 @@ public class Rule
     [Key]
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(200)]
     public string Name { get; set; }
+
+    public bool IsMandatory { get; set; }
+
+    public int? CreatedByUserId { get; set; }
 
     public List<RoomRule> RoomRules { get; set; } = new();
 }
